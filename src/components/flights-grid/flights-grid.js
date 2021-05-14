cc0ae01280c4f9c3afefc3a2db5cf77e509c05d2
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-05-14 19:59:29 +0530

    Initial commit

--- src/components/flights-grid/flights-grid.js
+++ src/components/flights-grid/flights-grid.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './flight-grid.css';
 import { FlightSearchInfo } from './../flight-search-info/flight-search-info';
 import { FlightInfo } from './../flight-info/flight-info';
-import { MultiFlightInfo } from './../multi-flight-info/multi-flight-info';
 
 
 const FlightsGrid = (props) => {
@@ -12,7 +11,6 @@ const FlightsGrid = (props) => {
     <div className="flights-info-container">
       {props.criteria && <FlightSearchInfo criteria={props.criteria} count={flightsCount || 0} />}
       {flights.nonStopFlights && flights.nonStopFlights.map(flight => <FlightInfo data={flight} />)}
-      {flights.multiStopFlights && flights.multiStopFlights.map(flight => <MultiFlightInfo data={flight} />)}
     </div>
   );
 }
