a029ab1cdbcc345e7e451d8db2a92345f48ace1c
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-05-14 17:13:45 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import rootSaga from './sagas';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+   reducer,
+   applyMiddleware(sagaMiddleware),
 );
+sagaMiddleware.run(rootSaga);
+
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+
 
